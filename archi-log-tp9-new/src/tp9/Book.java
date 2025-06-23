909b84c5f25acff891637506ce1b167e7c15b7fe
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2025-06-23 14:50:00 +0200

    Add getters and setters for Book properties; create vcs.xml for version control setup

--- archi-log-tp9-new/src/tp9/Book.java
+++ archi-log-tp9-new/src/tp9/Book.java
@@ -7,9 +7,31 @@ public class Book implements IBook {
 
     public Book (String title, double price)
     {
-        //TODO à compléter
-
+        this.title = title;
+        this.price = price;
     }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
     // getters, setters, etc.
 
 
